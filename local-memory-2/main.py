1ae2d97061b800fb6db97a70e8d0425427f6c07b
Jokeren <kerenzhou@openai.com>
2023-04-05 00:15:43 -0700

    Update

--- local-memory-2/main.py
+++ local-memory-2/main.py
@@ -4,10 +4,52 @@ import triton
 import triton.language as tl
 
 
+@triton.jit
+def matmul_core(
+    pid_m, pid_n, pid_z,
+    # Pointers to matrices
+    A, B, C,
+    # Matrix dimensions
+    M, N, K,
+    # The stride variables represent how much to increase the ptr by when moving by 1
+    # element in a particular dimension. E.g. stride_am is how much to increase a_ptr
+    # by to get the element one row down (A has M rows)
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+    TILE_M: tl.constexpr, TILE_N: tl.constexpr
+):
+    # do matrix multiplication
+    rm = pid_m * BLOCK_SIZE_M + tl.arange(0, TILE_M)
+    rn = pid_n * BLOCK_SIZE_N + tl.arange(0, TILE_N)
+    rk = pid_z * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
+    ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_SIZE_M), TILE_M)
+    rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_SIZE_N), TILE_N)
+    # pointers
+    A0 = A + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
+    B0 = B + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
+    acc = tl.zeros((TILE_M, TILE_N), tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        k_remaining = K - k * (BLOCK_SIZE_K)
+        a = tl.load(A0, mask=rk[None, :] < k_remaining, other=0.)
+        b = tl.load(B0, mask=rk[:, None] < k_remaining, other=0.)
+        acc += tl.dot(a, b, out_dtype=tl.float32)
+        A0 += BLOCK_SIZE_K * stride_ak
+        B0 += BLOCK_SIZE_K * stride_bk
+    acc = acc.to(C.dtype.element_ty)
+    # rematerialize rm and rn to save registers
+    rm = pid_m * BLOCK_SIZE_M + tl.arange(0, TILE_M)
+    rn = pid_n * BLOCK_SIZE_N + tl.arange(0, TILE_N)
+    C0 = C + (rm[:, None] * stride_cm + rn[None, :] * stride_cn)
+    mask = (rm < M)[:, None] & (rn < N)[None, :]
+    tl.store(C0, acc, mask=mask)
+
+
 @triton.autotune(
     configs=[
-        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256,
-                      'BLOCK_SIZE_K': 64, 'GROUP_SIZE_M': 8}, num_stages=3, num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64,
+                      'GROUP_SIZE_M': 8, 'TILE_M': 16, 'TILE_N': 16}, num_stages=3, num_warps=8),
     ],
     key=['M', 'N', 'K'],
 )
@@ -25,8 +67,7 @@ def matmul_kernel(
     stride_cm, stride_cn,
     # Meta-parameters
     BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
-    GROUP_SIZE_M: tl.constexpr,
-    ACTIVATION: tl.constexpr,
+    GROUP_SIZE_M: tl.constexpr, TILE_M: tl.constexpr, TILE_N: tl.constexpr
 ):
     # matrix multiplication
     pid = tl.program_id(0)
@@ -39,30 +80,22 @@ def matmul_kernel(
     group_size = min(grid_m - group_id * GROUP_SIZE_M, GROUP_SIZE_M)
     pid_m = group_id * GROUP_SIZE_M + (pid % group_size)
     pid_n = (pid % width) // (group_size)
-    # do matrix multiplication
-    rm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-    rn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_SIZE_M), BLOCK_SIZE_M)
-    rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_SIZE_N), BLOCK_SIZE_N)
-    rk = pid_z * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
-    # pointers
-    A = A + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
-    B = B + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
-    acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), tl.float32)
-    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
-        k_remaining = K - k * (BLOCK_SIZE_K)
-        a = tl.load(A, mask=rk[None, :] < k_remaining, other=0.)
-        b = tl.load(B, mask=rk[:, None] < k_remaining, other=0.)
-        acc += tl.dot(a, b, out_dtype=tl.float32)
-        A += BLOCK_SIZE_K * stride_ak
-        B += BLOCK_SIZE_K * stride_bk
-    acc = acc.to(C.dtype.element_ty)
-    # rematerialize rm and rn to save registers
-    rm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-    rn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    C = C + (rm[:, None] * stride_cm + rn[None, :] * stride_cn)
-    mask = (rm < M)[:, None] & (rn < N)[None, :]
-    tl.store(C, acc, mask=mask)
+    if (M - pid_m * BLOCK_SIZE_M < BLOCK_SIZE_M) and (N - pid_n * BLOCK_SIZE_N < BLOCK_SIZE_N):
+        matmul_core(pid_m, pid_n, pid_z, A, B, C, M, N, K, stride_am, stride_ak, stride_bk, stride_bn, stride_cm, stride_cn,
+                    BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N, BLOCK_SIZE_K=BLOCK_SIZE_K,
+                    TILE_M=TILE_M, TILE_N=TILE_N)
+    elif (M - pid_m * BLOCK_SIZE_M < BLOCK_SIZE_M) and (N - pid_n * BLOCK_SIZE_N >= BLOCK_SIZE_N):
+        matmul_core(pid_m, pid_n, pid_z, A, B, C, M, N, K, stride_am, stride_ak, stride_bk, stride_bn, stride_cm, stride_cn,
+                    BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N, BLOCK_SIZE_K=BLOCK_SIZE_K,
+                    TILE_M=TILE_M, TILE_N=BLOCK_SIZE_N)
+    elif (M - pid_m * BLOCK_SIZE_M >= BLOCK_SIZE_M) and (N - pid_n * BLOCK_SIZE_N < BLOCK_SIZE_N):
+        matmul_core(pid_m, pid_n, pid_z, A, B, C, M, N, K, stride_am, stride_ak, stride_bk, stride_bn, stride_cm, stride_cn,
+                    BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N, BLOCK_SIZE_K=BLOCK_SIZE_K,
+                    TILE_M=BLOCK_SIZE_M, TILE_N=TILE_N)
+    else:
+        matmul_core(pid_m, pid_n, pid_z, A, B, C, M, N, K, stride_am, stride_ak, stride_bk, stride_bn, stride_cm, stride_cn,
+                    BLOCK_SIZE_M=BLOCK_SIZE_M, BLOCK_SIZE_N=BLOCK_SIZE_N, BLOCK_SIZE_K=BLOCK_SIZE_K,
+                    TILE_M=BLOCK_SIZE_M, TILE_N=BLOCK_SIZE_N)
 
 
 def matmul(a, b, activation=None):
@@ -84,15 +117,14 @@ def matmul(a, b, activation=None):
         M, N, K,
         a.stride(0), a.stride(1),
         b.stride(0), b.stride(1),
-        c.stride(0), c.stride(1),
-        ACTIVATION=activation,
+        c.stride(0), c.stride(1)
     )
     return c
 
 
 torch.manual_seed(0)
-a = torch.randn((512, 512), device='cuda', dtype=torch.float16)
-b = torch.randn((512, 512), device='cuda', dtype=torch.float16)
+a = torch.randn((8193, 8192), device='cuda', dtype=torch.float16)
+b = torch.randn((8192, 8193), device='cuda', dtype=torch.float16)
 triton_output = matmul(a, b, activation=None)
 torch_output = torch.matmul(a, b)
 print(f"triton_output={triton_output}")
@@ -123,8 +155,8 @@ else:
     )
 )
 def benchmark(M, N, K, provider):
-    a = torch.randn((M, K), device='cuda', dtype=torch.float16)
-    b = torch.randn((K, N), device='cuda', dtype=torch.float16)
+    a = torch.randn((M, 8192), device='cuda', dtype=torch.float16)
+    b = torch.randn((8192, N), device='cuda', dtype=torch.float16)
     if provider == 'cublas':
         ms, min_ms, max_ms = triton.testing.do_bench(
             lambda: torch.matmul(a, b), rep=100)
